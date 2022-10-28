49771099a01a7bc0bf6f7b12eb9f59e8008fec6a
Djalma Feitosa <djalmaf00@gmail.coma>
2022-10-28 17:24:41 -0300

    Acrescentando comentários aos Posts

--- src/main/java/com/saledro/workshopmongo/domain/Post.java
+++ src/main/java/com/saledro/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.saledro.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.saledro.workshopmongo.dto.AuthorDTO;
+import com.saledro.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -65,17 +70,25 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 	
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -89,5 +102,6 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 }
